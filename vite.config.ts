18cad91c75e65cb73001c996d8c50c72f4ed6e2c
Etsi0 <albinkarlsson2003@gmail.com>
2025-03-10 08:29:44 +0100

    put the manifest in the wrong place

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,41 @@ export default defineConfig({
 	plugins: [
 		VitePWA({
 			registerType: "autoUpdate",
+			manifest: {
+				name: "Phadonia Search",
+				short_name: "Search",
+				theme_color: "hsl(240deg 69% 61%)",
+				background_color: "hsl(240deg 16% 95%)",
+				display: "standalone",
+				start_url: "/",
+				icons: [
+					{
+						src: "icon1.ico",
+						sizes: "16x16",
+						type: "image/x-icon"
+					},
+					{
+						src: "icon2.ico",
+						sizes: "32x32",
+						type: "image/x-icon"
+					},
+					{
+						src: "icon3.ico",
+						sizes: "64x64",
+						type: "image/x-icon"
+					},
+					{
+						src: "icon4.ico",
+						sizes: "128x128",
+						type: "image/x-icon"
+					},
+					{
+						src: "icon5.ico",
+						sizes: "256x256",
+						type: "image/x-icon"
+					}
+				]
+			},
 			workbox: {
 				globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'],
 				runtimeCaching: [
